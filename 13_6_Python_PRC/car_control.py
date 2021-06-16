47c870cb7b2c4849b2fdc853743051ee695fdd2b
Sophia <Sophias94171@gmail.com>
2021-06-15 22:34:40 -0700

    upload 0616

--- 13_6_Python_PRC/car_control.py
+++ 13_6_Python_PRC/car_control.py
@@ -13,17 +13,13 @@ w = 11.5
 # d2 = float(input('Enter d2 : '))
 west = 0
 d1 = 7.6 #7.6 #13
-d2 = 5.6 # 9 # 5.6
+d2 = 5.6 # 5.6 #9 
 print(f"Setup: (d1, d2) ={d1, d2}")
 
 depth = 18+d1-w
 rv = 4
-cir_time = 13/rv
-back_time1 = (d2+1.5)/rv
-back_time2 = depth/rv # speed = 80
-
 speed = 50
-print(cir_time, back_time1, back_time2) 
+# print(cir_time, back_time1, back_time2) 
 class _Getch:
 
     def __call__(self):
@@ -68,36 +64,38 @@ def get():
             print ("up")
             reverse_flag = 0
             s.write("/goStraight/run 80 \n".encode())
+            time.sleep(1)
+            s.write("/stop/run \n".encode())
 
         if k3=='B':
 
             print ("down")
             reverse_flag = 0
             s.write("/goStraight/run -80 \n".encode())
+            time.sleep(1)
+            s.write("/stop/run \n".encode())
 
         if k3=='C':
 
             print ("right")
             reverse_flag = 0
             s.write("/turn/run 80 -0.3 \n".encode())
+            time.sleep(1)
+            s.write("/stop/run \n".encode())
 
         if k3=='D':
 
             print ("left")
             reverse_flag = 0
             s.write("/turn/run 80 0.3 \n".encode())
-        
-        if k3=='P': # press f1
-
-            print ("reverse")
-            # reverse_flag = 1 
-            # s.write("/turn/run -80 0.2 \n".encode())
-            
-            s.write(f"/goStraight/run -{speed} \n".encode())
-            time.sleep(back_time)
-            s.write("/stop/run \n".encode())
+            time.sleep(1)
+            s.write("/stop/run \n".encode()) 
 
         if k3=='Q': # press f2
+            # 
+            back_time1 = (d2-1)/rv
+            cir_time = 10/rv
+            back_time2 = (depth-5)/rv # speed = 80
             print ("Backward")
             s.write(f"/goStraight/run -{speed} \n".encode())
             time.sleep(back_time1)
@@ -114,12 +112,23 @@ def get():
             time.sleep(back_time2)
             s.write("/stop/run \n".encode())
             time.sleep(1)
+        
+        if k3=='P': # press f1
+
+            print ("LineFollowMode")
+            
+            s.write(f"/Line/run 15 -80\n".encode())
+
+
+        if k3=='R': # press f3
+            print ("TagFollowMode")
+            s.write(f"/Tag/run \n".encode())
             
             
             
         
-        time.sleep(1)
-        s.write("/stop/run \n".encode())
+        
+        print("End Command (Tag, Line may not finish yet)")
         # if reverse_flag == 1:
         #     reverse_flag = 0
         #     s.write("/goStraight/run -80 \n".encode())
--- 13_7_Control_BBCar_by_Xbee/main.cpp
+++ 13_7_Control_BBCar_by_Xbee/main.cpp
@@ -6,76 +6,160 @@
 Ticker servo_ticker;
 
 PwmOut pin5(D5), pin6(D6);
+DigitalInOut pin8(D8);
 
 BufferedSerial xbee(D1, D0);
 BufferedSerial uart(D10, D9); //tx,rx
 BBCar car(pin5, pin6, servo_ticker);
+parallax_ping ping1(pin8);
 
-void follow(int x1, int x2){
-   int diff = x1 - x2 - 25;
-   printf("Diff = %d\n", diff);
-   if(diff<2 && diff>-2){
-      car.goStraight(-180);
-   }
-   else if(diff>0){
-      car.turn(-180, -0.3);
+void Line(Arguments *in, Reply *out);
+void Tag(Arguments *in, Reply *out);
+void mystop(Arguments *in, Reply *out);
+RPCFunction rpcLine (&Line, "Line");
+RPCFunction rpcTag (&Tag, "Tag");
 
+
+void track(int deg){
+   int speed = -80;
+   //deg = 0;
+   if(deg <1 && deg >-1){
+      car.goStraight(speed);
+      printf("straight\r\n");
    }
-   else{
-      car.turn(-180, 0.3);
+   else if(deg<0){
+      car.turn(speed, 0.6);
+      printf("left\r\n");
    }
-   ThisThread::sleep_for(150ms);
+   else {
+      car.turn(speed, -0.6);
+      printf("right\r\n");
+   }
+   ThisThread::sleep_for(300ms);
    car.stop();
-   ThisThread::sleep_for(400ms);
+   ThisThread::sleep_for(500ms);
 }
 
-int x[4];
-int main() {
-   int end = 20;
-   char buf[256], outbuf[256];
-   int step =0;
-   FILE *devin = fdopen(&xbee, "r");
-   FILE *devout = fdopen(&xbee, "w");
-   uart.set_baud(9600);
-   printf("Board !!!! im here\r\n");
-   // car.turn(-160, -0.3);
-   // ThisThread::sleep_for(600ms);
-   // car.stop();
-   // car.goStraight(-160);
-   // ThisThread::sleep_for(1500ms);
-   // car.stop();
+void line_track(int end, int speed){
+   char o = '1';
+   uart.write(&o, 1);
+   int step = 0;
+   int x[4];
    while (step<end) {
       if(uart.readable()){
-         for (int i=0;;i++){
-            char recv[1];
-            uart.read(recv, sizeof(recv));
-            x[i] = recv[0];
-            if(recv[0] =='\n'){
-               //printf("\r\n");
-               break;
+         x[0] = 0;
+         while(uart.readable()){
+            for (int i=0;;i++){
+               char recv[1];
+               uart.read(recv, sizeof(recv));
+               x[i] = recv[0];
+               if(recv[0] =='\n'){
+                  //printf("\r\n");
+                  break;
+               }
+               // printf("%d, ", recv[0]);
             }
-            printf("%d, ", recv[0]);
          }
-         printf("Current line(x1, x2, y1, y2): %d, %d, %d, %d \r\n", x[0], x[1], x[2], x[3]);
-         if(x[2]>0){
-            printf("Go run\r\n");
-            follow(x[0], x[1]);
+         int rho = x[0] - 85;
+         printf("Current rho %d, %d \r\n", x[0], rho);
+         if(rho<50 && rho>-50){
+            if(rho<1 && rho>-1)
+               car.goStraight(speed);
+            else if(rho>0){
+               car.turn(speed, -0.6);
+               printf("left\r\n");
+            }
+            else{
+               car.turn(speed, 0.6);
+               printf("right\r\n");
+            }
+            ThisThread::sleep_for(500ms);
+            car.stop();
+            ThisThread::sleep_for(500ms);
             step++;
          }
       }
    }
-   printf("End run \r\n");
+}
+void tag_track(int limit){
+   char o = '0';
+   uart.write(&o, 1);
+   int distance = 0;
+   int degree = -1;
+   int first =0;
+   int x[2];
+   char recv[1];
+   while (1){
+      if(uart.readable()){
+         degree = -1;
+         first = 0;
+         while(uart.readable()){
+            for (int i=0;;i++){
+               uart.read(recv, sizeof(recv));
+               if(recv[0] =='\n')
+                  break;
+               else{
+                  x[0] = recv[0];
+                  // printf("%d, ", recv[0]);
+               }  
+            }
+            degree = x[0] - 180;
+         }
+         // printf("=== %d\r\n", degree);
+         
+         distance = (float)ping1;
+         if(degree<30 && degree> -30){
+            printf("Current Status (deg, dist): %d, %d\r\n", degree, distance);
+            if(distance>limit){
+               printf("--> run track ");
+               track(degree);
+            }
+            else{
+               printf("--> stop at limit\r\n");
+               break;
+            }
+
+         }
+         else
+            car.stop();            
+      }    
+   }
 }
 
-      // printf("Board !!!! im here\r\n");
-      // memset(buf, 0, 256);
+void Line(Arguments *in, Reply *out){
+   int in0 = in->getArg<int>();
+   int in1 = in->getArg<int>();
+   line_track(in0, in1);
+}
 
-      // for( int i = 0; ; i++ ) {
-      //    char recv = fgetc(devin);
-      //    if(recv == '\n') {
-      //       printf("\r\n");
-      //       break;
-      //    }
-      //    buf[i] = fputc(recv, devout);
-      // }
-      // RPC::call(buf, outbuf);
+void Tag(Arguments *in, Reply *out){
+   tag_track(25);
+}
+
+int main() {
+   char buf[256], outbuf[256];
+   
+   FILE *devin = fdopen(&xbee, "r");
+   FILE *devout = fdopen(&xbee, "w");
+   uart.set_baud(9600);
+   printf("Board !!!! im here\r\n");
+   //line_track(15, -80);
+   //tag_track(25);
+   while(1){
+      memset(buf, 0, 256);
+      for( int i = 0; ; i++ ) {
+         char recv = fgetc(devin);
+         if(recv == '\n') {
+            printf("\r\n");
+            break;
+         }
+         buf[i] = fputc(recv, devout);
+      }
+      RPC::call(buf, outbuf);
+   }
+   // ------------ track line
+   // line_track(20);
+   // ------------ track Tag
+   // tag_track(25);
+}
+
